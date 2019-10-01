2794f46d37d03f9f85f0012f13f908e65faa4b1a
RW <rinze.watanabe.translation@gmail.com>
2019-10-02 00:45:34 +1000

    Renamed files

---
+++ 102.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        ans, level = [], [root]
+        while root and level:
+            ans.append([node.val for node in level])            
+            level = [kid for n in level for kid in (n.left, n.right) if kid]
+        return ans
