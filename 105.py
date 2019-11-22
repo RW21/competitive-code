9af8bb4858ad714296ba375c10dc61c2a6ab8680
RW <rinze.watanabe.translation@gmail.com>
2019-11-22 12:37:52 +1000

    Rename

--- 105.py
+++
@@ -1,9 +0,0 @@
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        
--- 70.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        a, b = 1, 2
-        for i in range(n -1 ):
-            a, b = b, a+b
-        return a
-
-print(Solution().climbStairs(500))
