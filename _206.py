fa6a2413bbd3c660fb4c9154e8dcac0a8e825d58
RW21 <rinze.watanabe.translation@gmail.com>
2019-12-17 14:46:50 +0900

    Update

--- _206.py
+++ _206.py
@@ -4,6 +4,7 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         current = head.next
@@ -12,9 +13,8 @@ class Solution:
         while current:
             temp = ListNode(current.val)
             temp.next = previous
-            
+
             previous = current
             current = current.next
-            
-        
-        return temp  
+
+        return temp
