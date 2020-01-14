1afcd062e671541d80dec3fa83788626bb601556
RW21 <rinze.watanabe.translation@gmail.com>
2020-01-14 21:47:01 +0900

    Update

---
+++ _414.py
@@ -0,0 +1,11 @@
+class Solution:
+    def thirdMax(self, nums):
+        m1 = m2 = m3 = -float("inf")
+        for num in nums:
+            if num > m1:
+                m1, m2, m3 = num, m1, m2
+            elif m2 < num < m1:
+                m2, m3 = num, m2
+            elif m3 < num < m2:
+                m3 = num
+        return m3 if m3 > -float("inf") else m1
