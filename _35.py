8841c47c1e645bd0521ba4f0c9696c651a11db52
RW21 <rinze.watanabe.translation@gmail.com>
2020-02-08 17:06:51 +0900

    Update

--- _35.py
+++ _35.py
@@ -1,5 +1,32 @@
 from typing import List
 
+def search_insert_binary_search(nums: List[int], target: int) -> int:
+    high = len(nums) - 1
+    low = 0
+    if nums[-1] < target:
+        return len(nums)
+    
+    if nums[-1] == target:
+        return len(nums) - 1
+    
+    if nums[0] >= target:
+        return 0
+
+    while high >= low:
+        mid = (low + high) //2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            if nums[mid - 1] < target:
+                return mid
+            high = mid - 1
+        else:
+            if nums[mid + 1] > target:
+                return mid + 1
+            low = mid + 1
+
+# print(search_insert_binary_search([0,1,2, 3,4,5,6,7,8], 7))
+print(search_insert_binary_search([1,4,6,7,8,9],6))
 
 def searchInsert(nums: List[int], target: int) -> int:
     # edge cases
@@ -19,4 +46,3 @@ def searchInsert(nums: List[int], target: int) -> int:
                 return i + 1
 
 
-print(searchInsert([1, 3], 3))
